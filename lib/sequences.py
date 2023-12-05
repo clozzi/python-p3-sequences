5743cd4be2f1de95f7eebc6acebff25fdecc95d9
Chris Lozzi <clozzi12@gmail.com>
2023-12-05 13:31:24 -0500

    all tests passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    if length <= 0:
+        print(fibonacci_list)
+    elif length == 1:
+        fibonacci_list.append(0)
+        print(fibonacci_list)
+    elif length == 2:
+        fibonacci_list = [0,1]
+        print(fibonacci_list)
+    else:
+        fibonacci_list = [0, 1, 1]
+        while len(fibonacci_list) < length:
+            next_num = fibonacci_list[-1] + fibonacci_list[-2]
+            fibonacci_list.append(next_num)
+        print(fibonacci_list)
